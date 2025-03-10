9354f25634dc0e48711afeb4ac42d006a77c55f3
ilonaSag <kupoles09@gmail.com>
2025-03-10 17:44:06 +0300

    Incoming.kt

--- Incoming.kt
+++ Incoming.kt
@@ -1,4 +1,17 @@
-class Incoming:Mobile() {
-    var incomName = ""
-    var incomNum = ""
+class Incoming(number:String,
+    date: String,
+    length: Double,
+               codCity: Int,):Mobile(number,
+    date,
+    length,
+    codCity) {
+    // вывод информации о звонке
+    override fun DisplayInfo() {
+        println("Входящий звонок:")
+        super.DisplayInfo()
+    }
+    //был ли звонок пропущен
+    fun Missed(): Boolean {
+        return length == 0.0
+    }
 }
