4de5771c06a3148e3f41fa12eaf968755f3215b8
ilonaSag <kupoles09@gmail.com>
2025-03-10 17:45:39 +0300

    pr9_A_v10_Sagutdinova.kt

--- pr9_A_v10_Sagutdinova.kt
+++ pr9_A_v10_Sagutdinova.kt
@@ -1,24 +1,27 @@
 fun main(){
     try {
-        var mobile = Mobile()
-        println("Выберите:")
-        println("1. Ввести свой телефонный звонок")
-        println("2. Ввести свой входящий звонок")
-        println("3. Ввести свой исходящий звонок")
-        println("4. Выход")
-        val n = readLine()!!.toInt()
-        when (n) {
-          1-> {
-              println("Введите номер теефона:")
-              mobile.number = readLine()!!.toString()
-              mobile.ValidNum(mobile.number)
-              println("Введите продолжительность разговора:")
-              mobile.length = readLine()!!.toDouble()
-              mobile.ValidLength(mobile.length)
-              println("Введите код города:")
-              mobile.codCity = readLine()!!.toInt()
-              mobile.ValidCod(mobile.codCity)
-          }
+        println("Введите номер:")
+        val number = readLine()!!.toString()
+        println("Введите дату в формате ГГГГ-ММ-ДД:")
+        val date = readLine()!!.toString()
+        println("Введите продолжительность разговора:")
+        val length = readLine()!!.toDouble()
+        println("Введите код города:")
+        val codCity = readLine()!!.toInt()
+        var mobile = Mobile(number,date,length,codCity)
+        var incoming = Incoming(number,date,length,codCity)
+        val rouming = true
+        var outgoing = Outgoing(number,length,date,codCity, rouming)
+        if (mobile.ValidNum(mobile.number)&&mobile.ValidDate(mobile.date)&&
+            mobile.ValidLength(mobile.length)&&mobile.ValidCod(mobile.codCity)) {
+            mobile.DisplayInfo()
+            mobile. Price()
+            println("\nДанные входящего звонка:")
+            incoming.DisplayInfo()
+            println("Пропущенный звонок: ${incoming.Missed()}")
+            println("\nДанные исходящего звонка:")
+            outgoing.DisplayInfo()
+            println("Долгий звонок: ${outgoing.LongCall()}")
 
         }
     }
