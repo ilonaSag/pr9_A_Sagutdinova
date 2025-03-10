4fb040d1cb1a7c4675ae36976ac10e2df2c4c3e8
ilonaSag <kupoles09@gmail.com>
2025-03-10 17:45:12 +0300

    Outgoing.kt

--- Outgoing.kt
+++ Outgoing.kt
@@ -1,4 +1,18 @@
-class Outgoing:Mobile() {
-    var outName = ""
-    var outNum = ""
+class Outgoing(
+    number: String,
+    length: Double,
+    date: String,
+    codCity: Int,
+    val isRoaming: Boolean
+) : Mobile(number, date, length,codCity) {
+    // вывод информации о звонке
+    override fun DisplayInfo() {
+        println("Исходящий звонок:")
+        super.DisplayInfo()
+        println("Роуминг: ${if (isRoaming) "Да" else "Нет"}")
+    }
+    // был ли звонок долгим
+    fun LongCall(): Boolean {
+        return length > 60
+    }
 }
